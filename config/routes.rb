58ccfdd9b6672a391a89c2f8735901d4a083c99a
dianakakoma <dikakoma@gmail.com>
2020-08-20 22:00:06 -0500

    created the 5 RESTful actions

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # EXAMPLE HTML ROUTE
+  # get "/photos" => "photos#index"
+
+  EXAMPLE JSON ROUTE WITH API NAMESPACE
+  namespace :api do
+    #   get "/photos" => "photos#index"
+    get "/reports" => "reports#index"
+    post "/reports" => "reports#create"
+    get "/reports/:id" => "reports#show"
+    patch "/reports/:id" => "reports#update"
+    delete "/reports/:id" => "reports#destroy"
+  end
+end
